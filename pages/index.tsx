60d3e5743d4e91c278b73958a0ec204583d28d2a
SwetaaRampariya <swetarampariya.shivinfotech@gmail.com>
2024-04-17 15:41:55 +0530

    package added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Barcode from './components/barcode'
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
